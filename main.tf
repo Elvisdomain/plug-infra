73c1b2a5b6beddc3b5f9fa5fc39170b71749c88e
Elvisdomain <87395778+Elvisdomain@users.noreply.github.com>
2024-11-05 07:41:06 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -37,23 +37,3 @@ module "alb" {
   ecs_service_name    = module.ecs.service_name
   target_group_arn    = module.ecs.target_group_arn
 }
-
-resource "aws_security_group" "my_sg" {
-  name        = "my-security-group"
-  description = "Security group for ECS"
-  vpc_id      = aws_vpc.my_vpc.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
