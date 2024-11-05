ddd9e57f7129a49027a4a8b4ef77146dfe5469a0
Elvisdomain <87395778+Elvisdomain@users.noreply.github.com>
2024-11-05 08:12:28 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,12 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.vpc.vpc_id
 }
 
 output "public_subnets" {
-  value = aws_subnet.public[*].id
+  value = module.vpc.public_subnet_ids
 }
 
 output "private_subnets" {
-  value = aws_subnet.private[*].id
+  value = module.vpc.private_subnet_ids
 }
+
